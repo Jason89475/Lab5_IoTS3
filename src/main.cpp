839531b50ae360f455e67addeba338357adf5782
Jason89475 <jasonc3n@gmail.com>
2026-09-24 18:29:49 -0400

    Finalization of IoT_S3_Lab05

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,91 @@
-#include <Arduino.h>
+#include <Arduino.h> 
 
-// put function declarations here:
-int myFunction(int, int);
+#include <SPI.h> 
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
+#include <SD.h> 
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+const int chipSelect = 10; 
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+File dataFile; 
+
+ 
+const int potPin = A0; 
+
+int sensorValue = 0; 
+
+
+
+int position = 0; 
+
+ 
+
+void setup() { 
+
+ //Start the SD card 
+
+  Serial.begin(9600); 
+//Initialize SD Card
+  Serial.println("Program Started"); 
+
+  Serial.println("Initializing SD Card..."); 
+
+  if (SD.begin(chipSelect)) { 
+
+   Serial.println("SD Card Found"); 
+
+  } 
+
+  else { 
+
+    Serial.println("SD Card Not Found"); 
+
+  } 
+
+//Write Header
+  dataFile = SD.open("sensor.csv", FILE_WRITE); 
+
+  dataFile.println("Time,Value"); 
+
+} 
+
+
+
+void loop() { 
+Serial.println(position); 
+//Read Potentiometer Value 
+int sensorValue = analogRead(potPin); 
+analogRead(A0); 
+sensorValue = analogRead(potPin); 
+
+ 
+
+Serial.print("Value = "); 
+
+Serial.println(sensorValue); 
+
+ 
+
+
+//Read Current Time 
+millis(); 
+//Open CSV File 
+
+SD.open("sensor.csv", FILE_WRITE); 
+  //Write Time and Value
+  dataFile.print("Time");
+  dataFile.print(millis()/1000);
+  dataFile.print(",");
+  dataFile.print("Value"); 
+  dataFile.println(sensorValue);
+
+ //Close File 
+dataFile.close(); 
+//Wait 1 Second
+delay(1000);
+} 
+
+ 
+
+
+
+ 
